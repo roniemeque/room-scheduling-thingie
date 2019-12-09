88078e3cf2202f5826bde5dc8bf390417d9e1f57
Ronie <roniemeque@icloud.com>
2019-12-08 22:41:31 -0300

    tagging events inside day blocks and other stuff

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,17 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 import moment from "moment";
+import PropTypes from "prop-types";
 
-const testEvents = [
-  {
-    startTime: "2019-12-09 10:00",
-    endTime: "2019-12-09 11:00"
-  }
-];
+const convertMinutesToPercentage = (minutes, total = 720) =>
+  (minutes * 100) / total;
 
 const DayStyled = styled.li`
   h2 {
     margin-bottom: 1.5rem;
+  }
+  .day-blocks {
+    position: relative;
   }
 `;
 
@@ -30,31 +30,72 @@ const Block = styled.li`
   }
 `;
 
-const Day = ({ date }) => {
+const EventBlock = styled.div`
+  position: absolute;
+  height: ${props => `${props.durationPercentage}%`};
+  top: ${props => `${props.startPercentage}%`};
+  width: 100%;
+  opacity: 0.5;
+  background: red;
+`;
+
+const Day = ({ date, events }) => {
   // generating 8 blocks for counting from 8h to 20h
   // since we are starting from 8, just add it
   const hourBlocks = Array.from({ length: 12 }, (_, i) => i + 8);
 
-  const eventBlocks = testEvents.map(testEvent => {
-    const dayStart = date.clone().hour(8);
-    console.log(dayStart.format("LLL"));
+  const eventsBlocks = events.map(event => {
+    const startPosMinutesPast = moment(event.startTime).diff(date, "minutes");
+    const eventDuration = moment(event.endTime).diff(
+      moment(event.startTime),
+      "minutes"
+    );
 
-    return { ...testEvent };
+    const startPercentage = convertMinutesToPercentage(startPosMinutesPast);
+    const durationPercentage = convertMinutesToPercentage(eventDuration);
+
+    return {
+      startPosMinutesPast,
+      eventDuration,
+      startPercentage,
+      durationPercentage,
+      ...event
+    };
   });
+
+  console.log(eventsBlocks);
 
   return (
     <DayStyled>
       <h2>{date.format("LL")}</h2>
-      <ul>
+      <ul className="day-blocks">
         {hourBlocks.map(hourBlock => (
           <Block>
             <div className="indicator">{hourBlock}</div>
             <div className="box"></div>
           </Block>
         ))}
+        {eventsBlocks.map(eventBlock => (
+          <EventBlock
+            startPercentage={eventBlock.startPercentage}
+            durationPercentage={eventBlock.durationPercentage}
+          ></EventBlock>
+        ))}
       </ul>
     </DayStyled>
   );
+};
+
+Day.propTypes = {
+  date: PropTypes.object.isRequired,
+  events: PropTypes.arrayOf(
+    PropTypes.shape({
+      startTime: PropTypes.string.isRequired,
+      endTime: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      owner: PropTypes.string.isRequired
+    })
+  )
 };
 
 export default Day;
--- src/components/DaysList.js
+++ src/components/DaysList.js
@@ -4,6 +4,27 @@ import styled from "styled-components";
 
 import Day from "./Day";
 
+const events = [
+  {
+    startTime: "2019-12-09 10:00",
+    endTime: "2019-12-09 11:30",
+    title: "Reunião legal",
+    owner: "Ronie"
+  },
+  {
+    startTime: "2019-12-09 15:00",
+    endTime: "2019-12-09 16:00",
+    title: "Reunião legal",
+    owner: "Ronie"
+  },
+  {
+    startTime: "2019-12-10 12:00",
+    endTime: "2019-12-10 13:00",
+    title: "Reunião legal",
+    owner: "Ronie"
+  }
+];
+
 const List = styled.ul`
   display: grid;
   grid-auto-flow: column;
@@ -14,16 +35,23 @@ const List = styled.ul`
 const DaysList = () => {
   // creating default days starting at 8 for comparison purposes
   const [days] = useState(
-    Array.from({ length: 3 }, (_, i) => moment().add(i, "days"))
-      .hour(8)
-      .minute(0)
+    Array.from({ length: 3 }, (_, i) =>
+      moment()
+        .add(i, "days")
+        .hour(8)
+        .minute(0)
+    )
   );
 
   return (
     <List>
-      {days.map(day => (
-        <Day key={day.unix()} date={day}></Day>
-      ))}
+      {days.map(day => {
+        const dayEvents = events.filter(
+          ({ startTime }) => day.format("L") === moment(startTime).format("L")
+        );
+
+        return <Day events={dayEvents} key={day.unix()} date={day}></Day>;
+      })}
     </List>
   );
 };
